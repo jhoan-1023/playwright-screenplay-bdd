88dc9967210258101506d39415345aa4cc21d3cd
Marquez Castrillon, Jhoan (Externo- AcidLabs) <jhoan.marquezcastrillon@externos-cl.cencosud.com>
2025-09-18 12:08:23 -0500

    se sube Question

--- src/questions/ModalError.ts
+++ src/questions/ModalError.ts
@@ -3,16 +3,11 @@ import { FiltrosUI } from '../ui/filtros';
 
 export class ModalError {
 
-    /**
-     * Esta es una Question que devuelve los textos del modal de error.
-     * @param page La página actual de Playwright.
-     * @returns Un objeto con el título y el mensaje del modal.
-     */
     static async getTexts(page: Page): Promise<{ title: string; message: string }> {
         await page.waitForSelector(FiltrosUI.errorModalContainer, { state: 'visible', timeout: 10000 });
         const title = await page.locator(FiltrosUI.errorModalTitle).innerText();
         const message = await page.locator(FiltrosUI.errorModalMessage).innerText();
-        console.log(`Question: Obteniendo textos del modal de error. Título: "${title}", Mensaje: "${message}"`);
+        console.log(`Obteniendo textos del modal de error. Título: "${title}", Mensaje: "${message}"`);
         return { title, message };
     }
 }
